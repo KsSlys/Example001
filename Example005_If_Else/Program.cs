7456507df6950cb146071274daebea784f90dd0f
«Ksenia» <ksenia_slys@mail.ru>
2023-02-10 17:05:33 +0300

    Example 005 If_Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console. WriteLine("Введите Ваше имя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ksenia")
+{
+    Console.WriteLine("Ура! Это же Ксюша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
