b83f363763349edb1942ec2b13e4b265f598953b
«Ksenia» <ksenia_slys@mail.ru>
2023-02-21 18:26:57 +0300

    Ex12

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,159 @@
+﻿// Методы, которые ничего не принимают и ничего не возвращают
+
+void Method1()
+{
+    Console.WriteLine("Автор Ксения");
+}
+
+//Method1(); // Как вызывать данный метод
+
+
+
+
+
+
+// Методы, которые ничего не возвращают, но могут принимать аргументы
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+//Method2(msg:"Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i =0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21(msg:"Текст", count: 4); 
+//Method21("Новый текст", 4); 
+
+
+
+
+
+
+// Методы, которые что-то возвращают, но ничего не принимают
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+//int year = Method3();
+//Console.WriteLine(year);
+
+
+
+
+
+
+// Методы, которые что-то принимают и что-то возвращают 
+
+// string Method4(int count, string c)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + c;
+//         i++;
+//     }
+//     return result;
+// }
+
+string Method4(int count, string c)
+{
+   string result = String.Empty;
+   for (int i = 0; i < count; i++)
+   {
+     result = result + c;
+   }
+    return result;
+}
+ string res = Method4(10, "qwerty text");
+// Console.WriteLine(res);
+
+
+
+// Цикл в цикле
+
+
+
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345     
+// s[3] // r       
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+// string newText = Replace(text, ' ', '|');
+
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+
+// Console.WriteLine();
+// newText = Replace(newText, 'с', 'С');
+// Console.WriteLine(newText);
+
+
+
+
+int[] arr = {1,5,8,3,6,1,7,9,2};
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    for (int i= 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)
+{
+
+    for (int i=0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j=i+1; j< array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array [minPosition];
+        array [minPosition] = temporary;
+
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
